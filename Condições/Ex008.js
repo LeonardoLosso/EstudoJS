43996ea4b4fdd5f6c10347530d4c47a65b785c26
Leonardo Losso <leonardolosso.sz@gmail.com>
2022-04-05 17:14:53 -0300

    ex 8 e 9

---
+++ Condições/Ex008.js
@@ -0,0 +1,16 @@
+var vel = 69
+
+console.log(`\nA velocidade do carro é ${vel}Km/h`)
+if (vel>60)
+{
+    console.log('você ultrapassou o limite!\n')
+}
+else if(vel<20)
+{
+    console.log('tu é uma tartaruga pacero\n')
+}
+else
+{
+    console.log('continue dirigindo com segurança\n')
+}
+//Igual em C
