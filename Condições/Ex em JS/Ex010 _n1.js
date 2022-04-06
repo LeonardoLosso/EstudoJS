1d729ac33592aace0ad66d8a058d0cefb3e1bf51
Leonardo Losso <leonardolosso.sz@gmail.com>
2022-04-06 01:19:38 -0300

    Ex 10
    ex 10 em html e js

---
+++ Condições/Ex em JS/Ex010 _n1.js
@@ -0,0 +1,17 @@
+var idade = 18
+
+if (idade>=16)
+{
+    if(idade>=18)
+    {
+        console.log('\nSeu voto é obrigatório!\n')
+    }
+    else
+    {
+        console.log('\nVoto opcional!\n')
+    }
+}
+else
+{
+    console.log('\nNão tem idade para votar\n')
+}
