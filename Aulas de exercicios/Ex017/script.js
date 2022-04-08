1ad08a3926c4b8bf675b7b16db97113a391b2928
Leonardo Losso <leonardolosso.sz@gmail.com>
2022-04-08 13:59:46 -0300

    ex 017 adicionado

--- Aulas de exercicios/Ex017/script.js
+++ Aulas de exercicios/Ex017/script.js
@@ -1,1 +1,24 @@
-alert('Olá')
+function tabuada()
+{
+    var entrada = document.getElementById('txtn')
+    var tab = document.getElementById('box')
+
+    if(entrada.value.length == 0)
+    {
+        alert('[ERRO]-bota um numero ai campeão')
+    }
+    else
+    {
+        let n = Number(entrada.value)
+        tab.innerHTML = ''
+        
+        for(var i=1; i<=10; i++)
+        {
+            let item = document.createElement('option')
+            item.text = `${n} x ${i} = ${n*i}`
+            item.value = `tab ${i}`//desnecessauro pro js
+            tab.appendChild(item)
+        }
+    }
+
+}
